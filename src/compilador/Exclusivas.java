731162c67cb6753b78dd2c043533a8ac4a448a00
Edu516 <54044456+Edu516@users.noreply.github.com>
2021-09-23 10:50:44 -0300

    Add files via upload

---
+++ src/compilador/Exclusivas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+import java.lang.reflect.Constructor;
+
+/**
+ *
+ * @author eduardo
+ */
+public class Exclusivas {
+    private static String exclusivas;
+    
+    public Exclusivas() {
+        exclusivas
+    }
+    
+
+    public String getExclusivas() {
+        return exclusivas;
+    }
+
+    public void setExclusivas(String exclusivas) {
+        Exclusivas.exclusivas = exclusivas;
+    }
+
+    
+    
+}
